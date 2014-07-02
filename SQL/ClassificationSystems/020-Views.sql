dcb76fade9bfcc66f86c4eaba237d1f3d239a75a
TonyBarnett <tony.barnett@ukplc.net>
2014-07-02 14:09:12 +0100

    updated definitions for classification systems database

--- SQL/ClassificationSystems/020-Views.sql
+++ SQL/ClassificationSystems/020-Views.sql
@@ -1,7 +1,8 @@
 CREATE VIEW AncestorValues
 AS
 WITH cteAncestorOrSelf AS (
-	SELECT strSystemId, strValue, strValue AS strAncestor, 0 AS intBlob FROM clasValue
+	SELECT strSystemId, strValue, strValue AS strAncestor, 0 AS intBlob 
+	FROM clasValue
 	
 	UNION ALL
 	
@@ -21,6 +22,26 @@ FROM (
 	SELECT a.strSystemId, a.strValue, a.strAncestor, m.*,
 		RANK() OVER (PARTITION BY a.strSystemId, a.strValue, m.strSystem2Id ORDER BY a.intBlob) AS intLevel
 	FROM AncestorValues a
-		INNER JOIN PotentialMatches m ON m.strSystem1Id = a.strSystemId AND m.strSystem1Value = a.strAncestor
+		INNER JOIN PotentialMatches m ON 
+			m.strSystem1Id = a.strSystemId AND 
+			m.strSystem1Value = a.strAncestor
 ) s
 WHERE s.intLevel = 1
+
+
+CREATE VIEW DecendantValues
+AS 
+WITH cteDescendants AS (
+	SELECT strSystemId, strValue, strValue AS strDescendant
+	FROM clasValue
+	
+	UNION ALL
+
+	SELECT cte.strSystemId, cte.strValue, v.strValue AS strDescendant
+	FROM cteDescendants cte
+		INNER JOIN clasValue v ON 
+			v.strSystemId = cte.strSystemId AND 
+			v.strParent = cte.strDescendant
+)
+SELECT * 
+FROM cteDescendants
